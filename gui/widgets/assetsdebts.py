c42d9abdf80b5f22e7a46a6a444078978c6dc2f9
wenbin wu <ahbenebha@gmail.com>
2021-05-21 22:38:41 -0400

    fixes

--- gui/widgets/assetsdebts.py
+++ gui/widgets/assetsdebts.py
@@ -11,10 +11,22 @@ class AssetsDebts():
         self.panel = panel
 
         self.splitter = wx.SplitterWindow(self.panel, wx.ID_ANY, style=wx.SP_LIVE_UPDATE)
+        self.left_pane = wx.Panel(self.splitter)
+        self.right_pane = wx.Panel(self.splitter)
 
-        self.panelLeft = wx.Panel(self.splitter)
-        wx.StaticText(self.panelLeft, -1, label='left side panel')
-        self.panelRight = wx.Panel(self.splitter)
-        wx.StaticText(self.panelRight, -1, label='right side panel')
+        self.assets_sizer = wx.StaticBoxSizer(wx.VERTICAL, self.left_pane, 'Assets')
+        self.debts_sizer = wx.StaticBoxSizer(wx.VERTICAL, self.right_pane, 'Debts')
 
-        self.splitter.SplitVertically(self.panelLeft, self.panelRight)
+        left_label = wx.StaticText(self.left_pane, -1, label='left side panel')
+        right_label = wx.StaticText(self.right_pane, -1, label='right side panel')
+
+        self.assets_sizer.Add(left_label)
+        self.debts_sizer.Add(right_label)
+        self.left_pane.SetSizer(self.assets_sizer)
+        self.right_pane.SetSizer(self.debts_sizer)
+        self.splitter.SplitVertically(self.left_pane, self.right_pane)
+        self.splitter.SetMinimumPaneSize(700)
+
+        widget_sizer = wx.BoxSizer(wx.VERTICAL)
+        widget_sizer.Add(self.splitter, 1, wx.EXPAND, 0)
+        self.panel.SetSizer(widget_sizer)
