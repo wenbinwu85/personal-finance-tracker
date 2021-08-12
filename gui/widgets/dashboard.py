b7fbd6824f5b03294d279a9504b838522074ef4f
wenbin wu <ahbenebha@gmail.com>
2021-08-11 22:06:58 -0400

    pie chart now reflects data from metrics dlvc row selection

--- gui/widgets/dashboard.py
+++ gui/widgets/dashboard.py
@@ -65,15 +65,16 @@ class Dashboard(wx.Panel):
         pie_legend = self.pie.GetLegend()
         pie_legend.SetTransparent(True)
         pie_legend.SetLabelColour(wx.Colour(225, 225, 225))
-        pie_part1 = PiePart(100, wx.Colour(200, 50, 50), 'Cash')
-        pie_part2 = PiePart(250, wx.Colour(50, 200, 50), 'Savings')
-        pie_part3 = PiePart(450, wx.Colour(50, 50, 200), 'Investments')
-        pie_part4 = PiePart(150, wx.Colour(200, 200, 50), 'Real Estate')
-
-        self.pie._series.append(pie_part1)
-        self.pie._series.append(pie_part2)
-        self.pie._series.append(pie_part3)
-        self.pie._series.append(pie_part4)
+        self.pie_part1 = PiePart(100, wx.Colour(200, 50, 50), 'TSP')
+        self.pie_part2 = PiePart(250, wx.Colour(50, 200, 50), 'Schwab')
+        self.pie_part3 = PiePart(450, wx.Colour(50, 50, 200), 'Roth IRA')
+        self.pie_part4 = PiePart(150, wx.Colour(200, 200, 50), 'Webull')
+        self.pie_part5 = PiePart(150, wx.Colour(200, 50, 200), 'Coinbase')
+        self.pie._series.append(self.pie_part1)
+        self.pie._series.append(self.pie_part2)
+        self.pie._series.append(self.pie_part3)
+        self.pie._series.append(self.pie_part4)
+        self.pie._series.append(self.pie_part5)
 
         self.hslider = wx.Slider(
             self, wx.ID_ANY, 180, 0, 360, size=(260, -1), style=wx.SL_LABELS | wx.SL_TOP
@@ -108,6 +109,7 @@ class Dashboard(wx.Panel):
         ]
         self.metrics_dvlc = dv.DataViewListCtrl(self.cpane.GetPane(), size=(860, 280), style=dv.DV_ROW_LINES)
         self.metrics_dvlc.Bind(dv.EVT_DATAVIEW_ITEM_CONTEXT_MENU, self.metrics_context_menu)
+        self.metrics_dvlc.Bind(dv.EVT_DATAVIEW_SELECTION_CHANGED, self.update_pie_chart)
         for i in metrics_columns:
             self.metrics_dvlc.AppendTextColumn(i, width=wx.COL_WIDTH_AUTOSIZE, mode=dv.DATAVIEW_CELL_EDITABLE)
         for item in load_data_from(METRICS_DATA_PATH):
@@ -123,6 +125,16 @@ class Dashboard(wx.Panel):
 
         self.hslider_handler(wx.EVT_SLIDER)
         self.vslider_handler(wx.EVT_SLIDER)
+        store = self.metrics_dvlc.GetStore()
+        last_row = store.GetItemCount()-1
+        last_row_data = [store.GetValueByRow(last_row, col) for col in range(store.GetColumnCount())]
+        self.pie_part1.SetValue(float(last_row_data[1]))
+        self.pie_part2.SetValue(float(last_row_data[2]))
+        self.pie_part3.SetValue(float(last_row_data[3]))
+        self.pie_part4.SetValue(float(last_row_data[4]))
+        self.pie_part5.SetValue(float(last_row_data[5]))
+        self.pie.Refresh()
+
 
     def vslider_handler(self, event):
         self.pie.SetAngle(float(self.vslider.GetValue()) / 180.0 * pi)
@@ -218,3 +230,15 @@ class Dashboard(wx.Panel):
         for i in range(row_count):
             data.append([self.metrics_dvlc.GetTextValue(i, j) for j in range(col_count)])
         dump_data(data, METRICS_DATA_PATH)
+
+    def update_pie_chart(self, event):
+        row = self.metrics_dvlc.GetSelectedRow()
+        store = self.metrics_dvlc.GetStore()
+        row_data = [store.GetValueByRow(row, col) for col in range(store.GetColumnCount())]
+
+        self.pie_part1.SetValue(float(row_data[1]))
+        self.pie_part2.SetValue(float(row_data[2]))
+        self.pie_part3.SetValue(float(row_data[3]))
+        self.pie_part4.SetValue(float(row_data[4]))
+        self.pie_part5.SetValue(float(row_data[5]))
+        self.pie.Refresh()
